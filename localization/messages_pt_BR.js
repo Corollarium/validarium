761238a1ff94fbbf6741f169fdd1a9c345662659
Bruno <brunobg@gmail.com>
2012-12-13 17:18:43 -0200

    Translation

---
+++ localization/messages_pt_BR.js
@@ -0,0 +1,11 @@
+/*
+* Translated default messages for the jQuery validation plugin.
+* Locale: PT (Portuguese; português)
+* Region: BR (Brazil)
+
+*/
+(function ($) {
+	$.extend($.validator.messages, {
+		required: "Este campo &eacute; requerido.",
+	});
+}(jQuery));
