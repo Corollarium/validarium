4835c1b4ea8a22816477aa2046dcafe70164e07e
Bruno <brunobg@gmail.com>
2012-12-14 12:04:54 -0200

    Fix, starting to run

--- src/jquery.validarium.js
+++ src/jquery.validarium.js
@@ -11,29 +11,30 @@
 
 		// check if validarium for this form was already created
 		// TODO: if applied to multiple items?
-		var validariumInstance = $.data(this[0], 'validarium');
-		if (validariumInstance) {
-			return validariumInstance;
-		}
 
-		return $(this).each(function() {
-			var instance = new $.validarium(settings, this);
-			$.data(this, 'validarium', instance);
+		var ret = [];
+		$(this).each(function() {
+			var instance = $.data(this, 'validarium');
+			if (!instance) {
+				instance = new $.validarium(settings, this);
+				$.data(this, 'validarium', instance);
+			}
+			ret.push(instance);
 		});
+		return ret;
 	};
 	
 $.validarium = function( options, form ) {
 	this.settings = $.extend( true, {}, $.validarium.defaults, options );
-
-	var elements = $(form)
-	.find("input, select, textarea")
-	.not(":submit, :reset, :image, [disabled]")
-	.not( this.settings.ignore );
+	this.elements = $(form)
+		.find("input, select, textarea")
+		.not(":submit, :reset, :image, [disabled]")
+		.not( this.settings.ignore );
 
 	// Add novalidate tag if HTML5.
 	$(form).attr('novalidate', 'novalidate');
 	
-	elements.each(function() {
+	this.elements.each(function() {
 		var element = this;
 		for (var i in element.attributes) {
 			if (i.substr(0, 10) == "data-rules") {
@@ -63,57 +64,71 @@ $.extend($.validarium, {
 			console.warn("Validarium:" + message);
 		}
 	},
-	
-	/**
-	 * Returns the value of an element, dealing with radio/checkbox/etc.
-	 * 
-	 * @return string
-	 */
-	elementValue: function(element) {
-		var type = $(element).attr('type'),
-			val = $(element).val();
 
-		if ( type === 'radio' || type === 'checkbox' ) {
-			return $('input[name="' + $(element).attr('name') + '"]:checked').val();
-		}
-
-		if ( typeof val === 'string' ) {
-			return val.replace(/\r/g, "");
-		}
-		return val;
-	},
-		
-	methods: {
-		required: function(value, element, param) {
-			if (element.nodeName.toLowerCase() === "select" ) {
-				// could be an array for select-multiple or a string, both are fine this way
-				var val = $(element).val();
-				return val && val.length > 0;
-			}
-			return $.trim(value).length > 0;
-		},	
-
-		minlength: function(value, element, param) {
-			// TODO
-			return $.trim(value).length >= param;
-		},
-
-		maxlength: function(value, element, param) {
-			// TODO
-			return $.trim(value).length <= param;
-		},
-
-		sameas: function(value, element, param) {
-			// TODO
-			return (value == $(param).val());
-		},
-		
-		regex: function(value, element, param) {
-			// TODO
-		},
-		
-		email: function(value, element, param) {
+	prototype: {
+		init: function() {
 			
+		},
+		
+		/**
+		 * Validates the form
+		 * @return boolean True if ok, false ir
+		 */
+		form: function() {
+			return false;
+		},
+		
+		/**
+		 * Returns the value of an element, dealing with radio/checkbox/etc.
+		 * 
+		 * @return string
+		 */
+		elementValue: function(element) {
+			var type = $(element).attr('type'),
+				val = $(element).val();
+	
+			if ( type === 'radio' || type === 'checkbox' ) {
+				return $('input[name="' + $(element).attr('name') + '"]:checked').val();
+			}
+	
+			if ( typeof val === 'string' ) {
+				return val.replace(/\r/g, "");
+			}
+			return val;
+		},
+			
+		methods: {
+			required: function(value, element, param) {
+				if (element.nodeName.toLowerCase() === "select" ) {
+					// could be an array for select-multiple or a string, both are fine this way
+					var val = $(element).val();
+					return val && val.length > 0;
+				}
+				return $.trim(value).length > 0;
+			},	
+	
+			minlength: function(value, element, param) {
+				// TODO
+				return $.trim(value).length >= param;
+			},
+	
+			maxlength: function(value, element, param) {
+				// TODO
+				return $.trim(value).length <= param;
+			},
+	
+			sameas: function(value, element, param) {
+				// TODO
+				return (value == $(param).val());
+			},
+			
+			regex: function(value, element, param) {
+				// TODO
+			},
+			
+			email: function(value, element, param) {
+				
+			}
 		}
 	}
 });
--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ module("validarium");
 test("Constructor", function() {
     var v1 = $("#testFormRequiredText").validarium();
     var v2 = $("#testFormRequiredText").validarium();
+    console.log(v1 == v2);
     equal( v1, v2, "Calling validarium() multiple times must return the same validator instance" );
 });
 
@@ -11,8 +12,8 @@ test("validarium() without elements, with non-form elements", 0, function() {
 });
 
 test("required(): text", function() {
-    expect( 3 );
+    expect( 1 );
     var form = $('#testFormRequiredText')[0];
-    var v = $(form).validarium();
+    var v = $(form).validarium()[0];
     ok( !v.form(), 'Invalid form' );
 });
