46368304e96af5efec912818f29ea6616a8e7c85
Bruno Roberto Búrigo <brunorb8@hotmail.com>
2017-03-23 15:36:50 -0300

    removing triggerHandler calls: it was never documented

--- test/test.js
+++ test/test.js
@@ -481,36 +481,6 @@ test("InvalidHandler and SubmitHandler: ", function() {
 	ok( submitHandler, 'Submit handler called');
 });
 
-
-test("InvalidHandlerField: ", function() {
-	expect( 6 );
-
-	var form = $('#testFormFocus');
-	var v = $(form).validarium()[0];
-
-	var qtd = 0;
-	form.find('input').one('invalid-field', function (){
-		qtd++;
-	});
-
-	ok( !v.form(), 'Invalid form' );
-	ok( qtd == 3, 'All indiviual handlers');
-
-	qtd = 0;
-	form.find('[name=f1],[name=f2]').one('invalid-field', function (){
-		qtd++;
-	});
-	var f1 = form.find('[name=f1]');
-	f1.val('something');
-	ok( !v.form(), 'Invalid form' );
-	ok( f1.hasClass('valid'), 'valid field 1' );
-	ok( form.find('[name=f2]').hasClass('error'), 'invalid field 2' );
-	equal( qtd, 1, '1 and 2 Individual handlers');
-
-	f1.val('');
-});
-
-
 test("Focus: ", function() {
 	expect( 12 );
 
