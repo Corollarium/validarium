d26f3bff9e1fd83c3a35683b631f6f4ec0fcb315
Bruno Roberto Búrigo <brunorb8@hotmail.com>
2017-04-05 16:33:23 -0300

    update deps version ; add jsdoc grunt task ; bump bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,8 @@
 module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-qunit');
-	
+	grunt.loadNpmTasks('grunt-jsdoc');
+
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		jshint: {
@@ -9,9 +10,18 @@ module.exports = function (grunt) {
 		},
 		qunit: {
 			all: ['test/index.html']
+		},
+		jsdoc : {
+			dist : {
+				src: ['src/jquery.validarium.js'],
+				options: {
+					destination: 'out'
+				}
+			}
 		}
 	});
-	
+
 	grunt.registerTask('test', ['qunit']);
+	grunt.registerTask('doc', ['jsdoc']);
 	grunt.registerTask('default', ['test']);
 };
